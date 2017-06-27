faae068ea67df2ff16e31915618fad9139d44461
xubiao <734425061@qq.com>
2017-06-27 19:04:37 +0800

    1.增加service-ribbon 客户端负载均衡

--- springcloud/servicehi/src/main/java/com/example/demo/ServiceHiApplication.java
+++ springcloud/servicehi/src/main/java/com/example/demo/ServiceHiApplication.java
@@ -4,14 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient  //启动EnableEureka客户端
+@EnableDiscoveryClient  //启动EnableEureka客户端,并向服务中心注册
 @RestController
 public class ServiceHiApplication {
 
@@ -19,14 +18,5 @@ public class ServiceHiApplication {
 		SpringApplication.run(ServiceHiApplication.class, args);
 		System.out.println("success springboot");
 	}
-
-
-	@Value("${server.port}")
-	String port;
-	@RequestMapping("/hi")
-	public String home(@RequestParam String name) {
-		return "hi "+name+",i am from port:" +port;
-	}
-
 
 }
